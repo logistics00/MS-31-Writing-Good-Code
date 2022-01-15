5866579bc2a47a577cc9a2e8f2ffdf4b74d20e58
Niek Mollers <info@sofane.nl>
2022-01-15 17:54:10 +0100

    528 Refactoring The CSRF Token Handling

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const db = require('./data/database');
 const blogRoutes = require('./routes/blog');
 const authRoutes = require('./routes/auth');
 const authMiddleware = require('./middlewares/auth-middleware');
+const addCSRFTokenMiddleware = require('./middlewares/csrf-token-middleware');
 
 const mongoDbSessionStore = sessionConfig.createSessionStore(session);
 const app = express();
@@ -22,6 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(session(sessionConfig.createSessionConfig(mongoDbSessionStore)));
 app.use(csrf());
 
+app.use(addCSRFTokenMiddleware);
 app.use(authMiddleware);
 
 app.use(blogRoutes);
