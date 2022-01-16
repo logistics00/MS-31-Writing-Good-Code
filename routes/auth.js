9027e38eeff5299d1059720dbf7b09f069c7d3c3
Niek Mollers <info@sofane.nl>
2022-01-16 01:36:46 +0100

    531 Protecting Routes With Custom Middleware

--- routes/auth.js
+++ routes/auth.js
@@ -8,5 +8,6 @@ router.get('/login', authControllers.getLogin);
 router.post('/signup', authControllers.signup);
 router.post('/login', authControllers.login);
 router.post('/logout', authControllers.logout);
+router.get('/401', authControllers.get401);
 
 module.exports = router;
